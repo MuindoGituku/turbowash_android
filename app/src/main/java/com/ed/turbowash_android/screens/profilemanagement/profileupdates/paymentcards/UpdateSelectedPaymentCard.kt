efee860dd7f49b06691c67b7ca5dd603687927f6
Emre Deniz <emre.deniz@outlook.com>
2024-04-14 11:22:47 -0400

    Internal documentation

--- app/src/main/java/com/ed/turbowash_android/screens/profilemanagement/profileupdates/paymentcards/UpdateSelectedPaymentCard.kt
+++ app/src/main/java/com/ed/turbowash_android/screens/profilemanagement/profileupdates/paymentcards/UpdateSelectedPaymentCard.kt
@@ -1,3 +1,8 @@
+/**
+ * @author Group 3 - Muindo Gituku, Emre Deniz, Nkemjika Obi
+ * @date Apr, 2024
+ */
+
 package com.ed.turbowash_android.screens.profilemanagement.profileupdates.paymentcards
 
 import androidx.compose.runtime.Composable
--- app/src/main/java/com/ed/turbowash_android/screens/profilemanagement/profileupdates/savedaddresses/ViewSavedAddressesScreen.kt
+++ app/src/main/java/com/ed/turbowash_android/screens/profilemanagement/profileupdates/savedaddresses/ViewSavedAddressesScreen.kt
@@ -1,3 +1,8 @@
+/**
+ * @author Group 3 - Muindo Gituku, Emre Deniz, Nkemjika Obi
+ * @date Apr, 2024
+ */
+
 package com.ed.turbowash_android.screens.profilemanagement.profileupdates.savedaddresses
 
 import androidx.compose.foundation.clickable
